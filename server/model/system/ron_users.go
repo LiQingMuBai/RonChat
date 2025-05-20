2b4b0083b397bad76a8caa129717229b80843c14
masion <cywhoyi@gmail.com>
2025-05-20 22:29:52 +0800

    feat(cache): implement local caching with go-cache
    Add in-memory cache using go-cache library with:
    - Default 30-minute expiration
    - Thread-safe operations
    - Basic CRUD operations (Set/Get/Delete/Flush)
    - Auto-cleanup of expired items

--- server/model/system/ron_users.go
+++ server/model/system/ron_users.go
@@ -8,18 +8,19 @@ import (
 // ronUsers表 结构体  RonUsers
 type RonUsers struct {
 	global.GVA_MODEL
-	Username  string `json:"username" form:"username" gorm:"comment:用户登录名;column:username;size:191;"`      //用户登录名
-	Uid       uint   `json:"uid" form:"uid" gorm:"comment:用户登录名;column:uid;size:191;"`                     //用户登录名
-	Password  string `json:"password" form:"password" gorm:"comment:用户登录密码;column:password;size:191;"`     //用户登录密码
-	Sex       int    `json:"sex" form:"sex" gorm:"comment:男女 1男 0女;column:sex;size:19;"`                   //男女 1男 0女
-	Enable    int    `json:"enable" form:"enable" gorm:"comment:用户是否被冻结 1正常 2冻结;column:enable;size:19;"`   //用户是否被冻结 1正常 2冻结
-	HeaderImg string `json:"headerImg" form:"headerImg" gorm:"comment:用户头像;column:header_img;size:191;"`   //用户头像
-	Phone     string `json:"phone" form:"phone" gorm:"comment:用户手机号;column:phone;size:191;"`               //用户手机号
-	Email     string `json:"email" form:"email" gorm:"comment:用户邮箱;column:email;size:191;"`                //用户邮箱
-	Telegram  string `json:"telegram" form:"telegram" gorm:"comment:用户telegram;column:telegram;size:191;"` //用户telegram
-	RoomId    string `json:"roomId" form:"roomId" gorm:"comment:roomID;column:room_id;size:191;"`          //roomID
-	Balance   string `json:"balance" form:"balance" gorm:"comment:balance;column:balance;size:191;"`       //balance
-	Anchor    int    `json:"anchor" form:"anchor" gorm:"comment: 1主播 0非主播;column:anchor;size:19;"`         // 1主播 0非主播
+	Username  string `json:"username" form:"username" gorm:"comment:用户登录名;column:username;size:191;"`          //用户登录名
+	Uid       uint   `json:"uid" form:"uid" gorm:"comment:用户登录名;column:uid;size:191;"`                         //用户登录名
+	Password  string `json:"password" form:"password" gorm:"comment:用户登录密码;column:password;size:191;"`        //用户登录密码
+	Sex       int    `json:"sex" form:"sex" gorm:"comment:男女 1男 0女;column:sex;size:19;"`                        //男女 1男 0女
+	Enable    int    `json:"enable" form:"enable" gorm:"comment:用户是否被冻结 1正常 2冻结;column:enable;size:19;"` //用户是否被冻结 1正常 2冻结
+	HeaderImg string `json:"headerImg" form:"headerImg" gorm:"comment:用户头像;column:header_img;size:191;"`        //用户头像
+	Phone     string `json:"phone" form:"phone" gorm:"comment:用户手机号;column:phone;size:191;"`                   //用户手机号
+	Email     string `json:"email" form:"email" gorm:"comment:用户邮箱;column:email;size:191;"`                     //用户邮箱
+	Telegram  string `json:"telegram" form:"telegram" gorm:"comment:用户telegram;column:telegram;size:191;"`        //用户telegram
+	RoomId    string `json:"roomId" form:"roomId" gorm:"comment:roomID;column:room_id;size:191;"`                   //roomID
+	StreamId  string `json:"streamId" form:"streamId" gorm:"comment:streamId;column:stream_id;size:191;"`           //roomID
+	Balance   string `json:"balance" form:"balance" gorm:"comment:balance;column:balance;size:191;"`                //balance
+	Anchor    int    `json:"anchor" form:"anchor" gorm:"comment: 1主播 0非主播;column:anchor;size:19;"`             // 1主播 0非主播
 }
 
 // TableName ronUsers表 RonUsers自定义表名 ron_users
